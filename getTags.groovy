0d7df87d2bd470d6a211dd24e55810dddeb248a7
flenters <flenters@gmail.com>
2020-07-16 06:38:43 +0200

    Create getTags.groovy

---
+++ getTags.groovy
@@ -0,0 +1,17 @@
+import groovy.json.JsonSlurper
+// GET
+try {
+    def get = new URL("http://yourinternalnexusurl:8018/applicacation/v1...etc").openConnection();
+    def getRC = get.getResponseCode();
+    if (getRC.equals(200)) {
+        def nexus_response = [:]
+        nexus_response = new JsonSlurper().parseText(get.getInputStream().getText())
+        def image_tag_list = []
+        for (tag in nexus_response.items.version){
+            image_tag_list.add(tag)
+        }
+       return image_tag_list.sort()
+    }
+}catch(Exception e){
+    println(e)
+}
